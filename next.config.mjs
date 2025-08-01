d152a5fe58f1b57e583db4e76896316a5e924228
SunnyCloudYang <sunnycloudyang@outlook.com>
2025-08-01 13:46:21 +0800

    build: build static pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
+const internalHost = process.env.TAURUS_DEV_HOST || 'localhost'
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +13,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
 }
 
 export default nextConfig
